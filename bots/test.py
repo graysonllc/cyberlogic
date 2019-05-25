c93bf2482624b4b960d0e163de141ee96265707d
graysonllc <nick@graysonllc.com>
2019-05-25 16:10:15 +0100

    very important

--- bots/test.py
+++ bots/test.py
@@ -1,26 +1,128 @@
+import argparse
+import heapq
+import configparser
+import os
+import sys
+import redis
+import memcache
+import ccxt
+
+config = configparser.ConfigParser()
+config.read('/root/akeys/b.conf')
+mysql_username=config['mysql']['MYSQL_USERNAME']
+mysql_password=config['mysql']['MYSQL_PASSWORD']
+mysql_hostname=config['mysql']['MYSQL_HOSTNAME']
+mysql_database='cmc'
+telegram_id=config['binance']['TEEGRAM_ID_EMBED']
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ['LD_LIBRARY_PATH'] = '/usr/local/lib'
+sys.path.append(root + '/python')
+	
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+
+def get_exchange():
+	
+	#Read in our apikeys and accounts
+	config = configparser.ConfigParser()
+	config.read('/root/akeys/b.conf')
+	conf=config['binance']
+	
+	binance_api_key=config['binance']['API_KEY']
+	binance_api_secret=config['binance']['API_SECRET']
+	
+	exchange = ccxt.binance({
+    'apiKey': binance_api_key,
+    'secret': binance_api_secret,
+    'enableRateLimit': True,
+    'rateLimit': 3600,
+    'verbose': False,  # switch it to Fal_se if you don't want the HTTP log
+	})
+	return(exchange)
+
+def fetch_prices(exchange, symbol):
+	ticker = exchange.fetch_ticker(symbol.upper())
+	return(ticker)
+
+
 def replace_last(source_string, replace_what, replace_with):
     head, _sep, tail = source_string.rpartition(replace_what)
     return head + replace_with + tail
 
-symbol=str('btc/usdt')
+
+def work_units(symbol,budget):
+
+	symbol=symbol.upper()
+	ticker_symbol=symbol
+
+	if symbol.endswith('BTC'):
+		ticker_symbol = replace_last(ticker_symbol, '/BTC', '')
+		trade_to='BTC'
+	elif symbol.endswith('USDT'):
+		ticker_symbol = replace_last(ticker_symbol, '/USDT', '')
+		trade_to='USDT'
+	elif symbol.endswith('BNB'):
+		ticker_symbol = replace_last(ticker_symbol, '/BNB', '')
+		trade_to='BNB'
+	elif symbol.endswith('TUSD'):
+		ticker_symbol = replace_last(ticker_symbol, '/TUSD', '')
+		trade_to='TUSD'
+	elif symbol.endswith('USD'):
+		ticker_symbol = replace_last(ticker_symbol, '/USD', '')
+		trade_to='USD'
+	elif symbol.endswith('USDC'):
+		ticker_symbol = replace_last(ticker_symbol, '/USDC', '')
+		trade_to='USDC'
+	elif symbol.endswith('PAX'):
+		ticker_symbol = replace_last(ticker_symbol, '/PAX', '')
+		trade_to='PAX'
+	elif symbol.endswith('USDS'):
+		ticker_symbol = replace_last(ticker_symbol, '/USDS', '')
+		trade_to='USDS'
+	elif symbol.endswith('ETH'):
+		ticker_symbol = replace_last(ticker_symbol, '/ETH', '')
+		trade_to='ETH'
+	
+	trade_from=ticker_symbol
+	exchange=get_exchange()
+	
+	if trade_to=='ETH':
+		tickers=fetch_prices(exchange,'ETH/USDT')
+		trade_to_price=float(tickers['close'])
+		tickers=fetch_prices(exchange,symbol)
+		pair_price=float(tickers['close'])
+		fraction_to_budget=budget/trade_to_price
+		units=fraction_to_budget/pair_price	
+	elif trade_to=='BTC':
+		tickers=fetch_prices(exchange,'BTC/USDT')
+		trade_to_price=float(tickers['close'])
+		tickers=fetch_prices(exchange,symbol)
+		pair_price=float(tickers['close'])
+		fraction_to_budget=budget/trade_to_price
+		units=fraction_to_budget/pair_price
+	elif trade_to=='BNB':
+		tickers=fetch_prices(exchange,'BNB/USDT')
+		trade_to_price=float(tickers['close'])
+		tickers=fetch_prices(exchange,symbol)
+		pair_price=float(tickers['close'])
+		fraction_to_budget=budget/trade_to_price
+		units=fraction_to_budget/pair_price
+	else:
+		units=budget/pair_price
+	
+	print("Trading From: "+str(trade_from))
+	print("Trade To: "+str(trade_to))
+	print(str(trade_to)+ "Price: "+str(trade_to_price))
+	print("Pair Price: "+str(pair_price))
+	print("Fraction Of "+str(trade_to)+" To "+str(budget)+" is: "+str(fraction_to_budget))
+	print("Units to Execute is: "+str(units))	
+	
+
+budget=100
+symbol=str('WABI/BTC')
 symbol=symbol.upper()
 ticker_symbol=symbol
-if symbol.endswith('BTC'):
-	ticker_symbol = replace_last(ticker_symbol, 'BTC', '')
-elif symbol.endswith('USDT'):
-	ticker_symbol = replace_last(ticker_symbol, '/USDT', '')
-elif symbol.endswith('BNB'):
-	ticker_symbol = replace_last(ticker_symbol, '/BNB', '')
-elif symbol.endswith('TUSD'):
-	ticker_symbol = replace_last(ticker_symbol, '/TUSD', '')
-elif symbol.endswith('USD'):
-	ticker_symbol = replace_last(ticker_symbol, '/USD', '')
-elif symbol.endswith('USDC'):
-	ticker_symbol = replace_last(ticker_symbol, '/USDC', '')
-elif symbol.endswith('PAX'):
-	ticker_symbol = replace_last(ticker_symbol, '/PAX', '')
-elif symbol.endswith('USDS'):
-	ticker_symbol = replace_last(ticker_symbol, '/USDS', '')
-elif symbol.endswith('ETH'):
-	ticker_symbol = replace_last(ticker_symbol, '/ETH', '')
-print(ticker_symbol)
+
+work_units(symbol,budget)
